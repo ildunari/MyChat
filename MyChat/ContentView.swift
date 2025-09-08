ebd3f95eb0fa84accbfc0df7297007046afd999b
ildunari <kosta963@gmail.com>
2025-09-08 17:33:49 -0400

    Initial Commit

---
+++ MyChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyChat
+//
+//  Created by Kosta Milovanovic on 9/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyChat/MyChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyChatApp.swift
+//  MyChat
+//
+//  Created by Kosta Milovanovic on 9/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MyChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

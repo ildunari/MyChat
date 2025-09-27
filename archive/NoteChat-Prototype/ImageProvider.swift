2271118484573a778f7c6edc3f57b5d4218d0666
ildunari <kosta963@gmail.com>
2025-09-27 14:00:03 -0400

    chore: remove legacy archive snapshots

--- archive/NoteChat-Prototype/ImageProvider.swift
+++
@@ -1,9 +0,0 @@
-// Providers/ImageProvider.swift
-import Foundation
-
-protocol ImageProvider {
-    var id: String { get }
-    var displayName: String { get }
-    func listModels() async throws -> [String]
-    func generateImage(prompt: String, model: String) async throws -> Data
-}
